8925ee51390e8d4d49372fd6f170327a070ce46d
Basile <Basile.thiry@hotmail.fr>
2021-12-02 22:10:12 +0100

    addUserForApi

---
+++ src/main/java/nuit/info/Quichtouille/model/User.java
@@ -0,0 +1,70 @@
+package nuit.info.Quichtouille.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class User {
+
+    private long id;
+    private String nom;
+    private String prenom;
+    private String role;
+    private String mdp;
+
+    public User(long id, String nom, String prenom, String role, String mdp) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.role = role;
+        this.mdp = mdp;
+    }
+
+    @Id
+    @GeneratedValue
+
+    @Column(name = "id", unique = true, nullable = false)
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name="nom")
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Column(name="prenom")
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    @Column(name="role")
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Column(name="mdp")
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+}
