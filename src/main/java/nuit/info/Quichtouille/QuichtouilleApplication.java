8b2e8d1cc11c61b298b825a779e99e3a822bdf30
tba5767 <theo.barbary@toyotafr.com>
2021-12-02 19:25:13 +0100

    init

---
+++ src/main/java/nuit/info/Quichtouille/QuichtouilleApplication.java
@@ -0,0 +1,13 @@
+package nuit.info.Quichtouille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuichtouilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuichtouilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nuit/info/Quichtouille/QuichtouilleApplicationTests.java
@@ -0,0 +1,13 @@
+package nuit.info.Quichtouille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuichtouilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
