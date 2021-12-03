0cf645ef7bf72ca136f402cecfdfe1b63356e909
Barbary-Theo <theo.barbary25@gmail.com>
2021-12-03 01:39:55 +0100

    wtf

---
+++ src/main/java/nuit/info/quichtouille/controller/CaptchaController.java
@@ -0,0 +1,20 @@
+package nuit.info.quichtouille.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/captcha")
+public class CaptchaController {
+
+
+    @GetMapping
+    public String getMappingPage(Model model) {
+
+        return "captcha";
+    }
+}
+
+
