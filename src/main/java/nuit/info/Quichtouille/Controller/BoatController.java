0f7e18eb46c75812c8504e9410a6cd068db056bf
Basile <Basile.thiry@hotmail.fr>
2021-12-03 03:41:16 +0100

    add boatcontroller

---
+++ src/main/java/nuit/info/Quichtouille/Controller/BoatController.java
@@ -0,0 +1,26 @@
+package nuit.info.quichtouille.controller;
+
+import nuit.info.quichtouille.model.Boat;
+import nuit.info.quichtouille.repositories.BoatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/boat")
+public class BoatController {
+
+    @Autowired
+    private BoatRepository boatRepository;
+
+
+    @PostMapping("/addBoat")
+    @ResponseBody
+    public Boat addBoat(@RequestBody Boat boat){
+        return boatRepository.save(boat);
+    }
+
+}
