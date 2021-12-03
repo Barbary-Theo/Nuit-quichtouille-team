10eaa18bc44eeedcabbcefc8089ab6abfab0eaa5
Basile <Basile.thiry@hotmail.fr>
2021-12-03 03:04:22 +0100

    test

--- src/main/java/nuit/info/quichtouille/controller/LoginController.java
+++ src/main/java/nuit/info/quichtouille/controller/LoginController.java
@@ -15,43 +15,5 @@ import java.util.Optional;
 @Controller
 public class LoginController {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @GetMapping
-    public String getMappingPage(Model model){
-        return "login";
-    }
-
-    @PostMapping("/addUser")
-    @ResponseBody
-    public User addUser(@RequestBody User user){
-        return userRepository.save(user);
-    }
-
-
-    @GetMapping("/getAll")
-    public ResponseEntity<List<User>> getAll(){
-        System.out.println("test");
-        userRepository.findAll().forEach(user -> {
-            System.out.println(user.getPrenom());
-        });
-
-
-        return ResponseEntity.ok(userRepository.findAll());
-
-    }
-
-    @GetMapping("/getById/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable(value = "id") long id) throws Exception {
-
-        User user = userRepository.findById(id).orElseThrow(() ->  new Exception("AIE"));
-        return ResponseEntity.ok(user);
-    }
-
-    @GetMapping("/geet")
-    public void geet(){
-        System.out.println("oui");
-    }
 
 }
