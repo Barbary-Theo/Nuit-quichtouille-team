b9a062d7acea9975e4d1a76fc94f89397b68a50e
Basile <Basile.thiry@hotmail.fr>
2021-12-03 02:58:13 +0100

    Ok it work ¯\_(ツ)_/¯

---
+++ src/main/java/nuit/info/Quichtouille/services/implementation/BoatServiceImpl.java
@@ -0,0 +1,45 @@
+package nuit.info.quichtouille.services.implementation;
+
+import nuit.info.quichtouille.model.Boat;
+import nuit.info.quichtouille.model.User;
+import nuit.info.quichtouille.repositories.BoatRepository;
+import nuit.info.quichtouille.services.BoatService;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+public class BoatServiceImpl implements BoatService {
+
+    @Resource
+    private BoatRepository boatRepository;
+
+    //Repository Getters / Setters
+    public BoatRepository getBoatRepository() {
+        return boatRepository;
+    }
+
+    public void setBoatRepository(BoatRepository boatRepository) {
+        this.boatRepository = boatRepository;
+    }
+
+
+    @Override
+    public Boat findById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Boat> findAll() {
+        return null;
+    }
+
+    @Override
+    public Boat save(User user) {
+        return null;
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+}
