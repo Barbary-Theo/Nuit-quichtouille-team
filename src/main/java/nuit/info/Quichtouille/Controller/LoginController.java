aa3d9c73b9b7939b8bf076a134351626f98b3195
Basile <Basile.thiry@hotmail.fr>
2021-12-03 02:26:54 +0100

    all is working :)

--- src/main/java/nuit/info/Quichtouille/Controller/LoginController.java
+++ src/main/java/nuit/info/Quichtouille/Controller/LoginController.java
@@ -1,16 +1,16 @@
 package nuit.info.Quichtouille.Controller;
 
+import net.bytebuddy.implementation.bytecode.Throw;
 import nuit.info.Quichtouille.model.User;
 import nuit.info.Quichtouille.repositories.UserRepository;
-import nuit.info.Quichtouille.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class LoginController {
@@ -31,12 +31,22 @@ public class LoginController {
 
 
     @GetMapping("/getAll")
-    public List<User> getAll(){
+    public ResponseEntity<List<User>> getAll(){
         System.out.println("test");
         userRepository.findAll().forEach(user -> {
             System.out.println(user.getPrenom());
         });
-        return userRepository.findAll();
+
+
+        return ResponseEntity.ok(userRepository.findAll());
+
+    }
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(value = "id") long id) throws Exception {
+
+        User user = userRepository.findById(id).orElseThrow(() ->  new Exception("AIE"));
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping("/geet")
